9318dd78d5c8e9be1952162e8cd50769cdb5c179
VibhavDhuri <vibhavpdhuri@gmail.com>
2022-07-27 00:40:04 +0530

    Added: UC1

--- Stack.java
+++ Stack.java
@@ -2,4 +2,13 @@ package com.stackandqueue;
 
 public class Stack {
 
+	LinkedList list = new LinkedList();
+
+	public void push(Object data) {
+		list.addFirst(data);
+	}
+
+	public void display() {
+		list.display();
+	}
 }
--- StackMain.java
+++ StackMain.java
@@ -3,8 +3,15 @@ package com.stackandqueue;
 public class StackMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Stack stack = new Stack();
+		System.out.println("Stack Adding:");
+		stack.push(70);
+		stack.display();
+		stack.push(30);
+		stack.display();
+		stack.push(56);
+		stack.display();
 	}
 
 }
